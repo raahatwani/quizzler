c512258557b563bd922577d824f88aefba753f56
Raahat Wani <raahatwani@gmail.com>
2023-11-19 12:31:54 +0530

    first commit

---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: empty_constructor_bodies
+
+class Question {
+  String questionText = '';
+  late bool questionAnswer;
+
+  Question({required this.questionText, required this.questionAnswer}) {
+  }
+}
